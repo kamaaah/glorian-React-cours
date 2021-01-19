d6eccd2e6cec489d05b44c311253e2a9679b8cb3
kmaadteck <x.argis@gmail.com>
2021-01-19 20:45:22 +0100

    modification des infos dans  test.js

--- test.js
+++ test.js
@@ -2,14 +2,13 @@
 // qd on a une variable structurée de pouvoir la décomposée et de récupérer les propriétés individuellement
 // constlisteNombre = [1, 2, 3, 4];
 
-
 console.log('***************  COurs Glorian *************************');
 // ******************* DESTRUCTURING ****************************
-let personnes = ["Pierre", "Paul", "Jacques"];
+let personnes = ['Pierre', 'Paul', 'Jacques'];
 // let personne1 = personnes[0];
 // let personne2 = personnes[1];
 // let personne3 = personnes[2];
-let animal = { chien: "bob", chat: "kitty" };
+let animal = { chien: 'bob', chat: 'kitty' };
 let [personne1, personne2] = personnes;
 // console.log(personne1, personne2);
 let { chat } = animal;
@@ -22,60 +21,64 @@ const tab2 = [1, 2, ...tab];
 // console.log(tab2);
 // DEFAULT PARAMETERS
 const somme = (a = 0, b = 0) => {
-    console.log(a, b);
-    return a + b;
+  console.log(a, b);
+  return a + b;
 };
 // console.log(somme(2, 5));
 //FUNCTION VARIADIQUE
 const saluer = (...prenom) => {
-    console.log(...prenom);
+  console.log(...prenom);
 };
 //BOUCLES FOR
-const listNumber = ["un", "deux", "trois"];
+
+const listNumber = ['un', 'deux', 'trois'];
 // listNumber List { 0: "un", 1: "deux", 2: "trois" }
-const objNumber = { 0: "un", 1: "deux", 2: "trois" };
+
+const objNumber = { 0: 'un', 1: 'deux', 2: 'trois' };
+console.log('********** listNumber ************');
 for (let n in listNumber) console.log(n);
 for (let n of listNumber) console.log(n);
+console.log(' ********** objNumber ************');
 for (let n in objNumber) console.log(objNumber[n]);
-for (let n of objNumber) console.log(n);
+// for (let n of objNumber) console.log(objNumber[0]);
 //CLASS
 class Animal {
-    constructor(prenom) {
-        this.prenom = prenom;
-    }
+  constructor(prenom) {
+    this.prenom = prenom;
+  }
 }
 class Carre {
-    constructor(objectColor) {
-        this.couleur = objectColor;
-        this.forme = "carre";
-        console.log(this); // 1
-        console.log(this.couleur); // 2
-    }
+  constructor(objectColor) {
+    this.couleur = objectColor;
+    this.forme = 'carre';
+    console.log(this); // 1
+    console.log(this.couleur); // 2
+  }
 }
 // // const c = new Carre("vert");
 // // console.log(c); // 1
 // // console.log(c.couleur); // 2
 // TEMPLATE LITERALS
-const nom = "Pierre";
+const nom = 'Pierre';
 const string = `"N'est-ce pas"`;
 console.log(`${string} ${nom}`);
 //MAP FILTER REDUCE
 const tableau = [1, 2, 3, 4, 5];
 console.log(
-    tableau.map(function(n, i) {
-        console.log(n, i);
-        return (n += 2);
-    })
+  tableau.map(function (n, i) {
+    console.log(n, i);
+    return (n += 2);
+  })
 );
 console.log(
-    tableau.filter(function(n) {
-        return n <= 3;
-    })
+  tableau.filter(function (n) {
+    return n <= 3;
+  })
 );
 console.log(
-    tableau.reduce(function(previous, current) {
-        return previous + current;
-    })
+  tableau.reduce(function (previous, current) {
+    return previous + current;
+  })
 );
 // console.log(tableau.reduce((previous, current) => previous + current));
 // console.log(tableau.map((n) => (n += 2)));
@@ -85,22 +88,19 @@ console.log(
 
 console.log('"################### ENd Cours Glorian #########################');
 
-
-
-
 // *********************** DEFAULT PARAMETERS ********************************
 // const somme = (a, b) => a + b;
 // console.log(somme(2)); // affiche not a number parce que la valeur de b n est pas définie
 
-somme = (a = 0, b = 0) => {
-    console.log(a, b);
-    return a + b;
-}
-console.log(somme(2, 5));
+sommes = (a = 0, b = 0) => {
+  console.log(a, b);
+  return a + b;
+};
+console.log(sommes(2, 5));
 
 // **************** REST parameter => permet de creér une FONCTION VARIADIQUE ******************
 // => qui peut avoir plusieurs arguments.
-// Il est utilisé dans la déclaration de la fonction 
+// Il est utilisé dans la déclaration de la fonction
 // dans la déclaration des araqguments passés en parametre en entrée de ma fonction
 
 // const saluer = (...prenom) => { // correspond au rest operateur
@@ -108,28 +108,24 @@ console.log(somme(2, 5));
 // }
 // console.log(...prenom);
 
-
 var params = [1, 2, 3, 4, 5, 11, 27, 28, 68];
-// let chiffre = params % 2;
+let chiffre = params % 2;
 
-// const recuperePairs = (x) => console.log(x);
-// console.log(chiffre);
+const recuperePairs = (x) => console.log(x);
+console.log(chiffre);
 
-// const recuperePairs = (...params) => { // 1er version
-//     const tab = [];
-//     for (n of params)
-//         if (!(n % 2)) tab.push(n);
-//     consol.log(tab);
-// }
-
-
+const recuperePaires = (...params) => { // 1er version
+    const tab = [];
+    for (n of params)
+        if (!(n % 2)) tab.push(n);
+    consol.log(tab);
+}
 
 // const recuperePairs = (...numbers) => {
 //     const result = (numbers.filter((n) => !(n % 2))); // 2ère refacto
 
 //     console.log(chiffres.filter % 2 === 0);
 // }
-
 
 // const recuperePais = (...numbers) => {
 //     console.log(numbers.filter((n) => !(n % 2)));
@@ -152,8 +148,6 @@ var params = [1, 2, 3, 4, 5, 11, 27, 28, 68];
 // for (let of listNumber) console.log(n);
 // for (let n in objNumber) console.log(objNumber[n]);
 
-
-
 // function somme(...params) {
 //     return params.reduce((accumulateur, element) => {
 //         return accumulateur + element;
@@ -163,150 +157,149 @@ var params = [1, 2, 3, 4, 5, 11, 27, 28, 68];
 // console.log(somme(1));
 // console.log(somme(1, 2, 3, 4, 5));
 
-class Carre {
-    constructor(objectColor, forme) {
-        console.log(this);
-        this.couleur = objectColor;
-        this.forme = "carre";
-        console.log(this);
-    }
-}
-const c = new Carre('vert');
-console.log(c);
-console.log(c.couleur);
+// class Rectangle {
+//     constructor(objectColor, forme) {
+//         console.log(this);
+//         this.couleur = objectColor;
+//         this.forme = "rectangle";
+//         console.log(this);
+//     }
+// }
+// const c = new Carre('vert');
+// console.log(c);
+// console.log(c.couleur);
 
 //*****************  Template literals => utilisation du backtick AltGr + 7 *******************
 const pers = {
-    prenom: 'john',
-    nom: 'Wick'
-}
+  prenom: 'john',
+  nom: 'Wick',
+};
 
 // const message = 'Bonjour' + pers.prenom + ' ' + pers.nom + ' !';
-message = `Bonjour ${pers.prenom} ${pers.nom} !`
-
+message = `Bonjour ${pers.prenom} ${pers.nom} !`;
+console.log(message);
 
 // *********** Exo by me *****************
 class guest {
-    constructor(invite, invite1, invite2) {
-        this.invite = "Jean";
-        this.invite1 = "Paul";
-        this.invite2 = "Jack";
-        console.log(this);
-    }
+  constructor(invite, invite1, invite2) {
+   this.invite = 'Jean';
+    this.invite1 = 'Paul';
+    this.invite2 = 'Jack';
+    console.log(this);
+  }
 }
 
-const saluerTous = (invite, invite1, invite2) => {
+const saluerTous = ()  => {
         console.log(`Bonjour à ${guest.invite}, ${guest.invite1}, ${guest.invite2}`);
         console.log(`Bonjour à ${guest.invite1} et ${guest.invite2}, ${guest.invite}`);
         console.log(`Bonjour à ${guest.invite2},  ${guest.invite} et ${guest.invite1}`);
     }
-    // const saluerTous = (invite = "Jean", invite1 = "Paul", invite2 = "Jack") => {
-    //     console.log(`Bonjour a ${invite}, ${invite1}, ${invite2} `);
-    //     console.log(`Bonjour a ${invite} et ${invite1}, ${invite} `);
-    //     console.log(`Bonjour a ${invite}, ${invite} et ${invite} `);
-    // }
+
+// const saluerTous = (invite = 'Jean', invite1 = 'Paul', invite2 = 'Jack') => {
+//   console.log(`Bonjour a ${invite}, ${invite1}, ${invite2} `);
+//   console.log(`Bonjour a ${invite} et ${invite1}, ${invite} `);
+//   console.log(`Bonjour a ${invite}, ${invite} et ${invite} `);
+// };
 saluerTous();
 
 //MAP  https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/map
-const tableau = [1, 2, 3, 4, 5];
+const array = [1, 2, 3, 4, 5];
 console.log(
-    tableau.map(function(element_courrant, index) {
-        console.log(element_courrant, index);
-        return (element_courrant);
-
-    })
+  array.map(function (element_courrant, index) {
+    console.log(element_courrant, index);
+    return element_courrant;
+  })
 );
 
 // Meme function écrite différement
 console.log(
-    tableau.map(function(n, i) {
-        console.log(n, i);
-        return (n += 2);
-    })
+  array.map(function (n, i) {
+    console.log(n, i);
+    return (n += 2);
+  })
 );
 
-// version a utiliser 
-console.log(tableau.map((n, i) => (n += 2)));
-
-
+// version a utiliser
+console.log(array.map((n, i) => (n += 2)));
 
 // FILTER  https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/filter
 console.log(
-    tableau.filter(function(n) {
-        returnn <= 3;
-    })
+  array.filter(function (n) {
+    return n <= 3;
+  })
 );
 
 console.log(
-    tableau.reduce(function(previous, current) {
-        return previous + current;
-    })
+  array.reduce(function (previous, current) {
+    return previous + current;
+  })
 );
 
 // Example Developer.molzilla.org
 const word = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-const result = words.filter(word => word.length > 6);
+const result = word.filter(word => word.length > 6);
 console.log(result); //expected outpu: Array["exuberant", "destruction", "present"]
-
 
 // le 19 Janvier 2021
 //fonction Asynchrone avec les promises
 // ###########   Glorian Bikoumou 9 h 36 #############""
-const get = (url) => {
-    return new Promise((resolve, reject) => {
-        const xhr = new window.XMLHttpRequest();
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4)
-                if (xhr.status === 200) resolve(xhr.responseText);
-                else reject("Une erreur est survenue");
-        };
-        xhr.open("GET", url);
-        xhr.send();
-    });
+const gets = url => {
+  return new Promise((resolve, reject) => {
+    const xhr = new window.XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4)
+        if (xhr.status === 200) resolve(xhr.responseText);
+        else reject('Une erreur est survenue');
+    };
+    xhr.open('GET', url);
+    xhr.send();
+  });
 };
-const getTodos = () => {
-    const response = get("https://jsonplaceholder.typicode.com/users")
-        .then((responseText) => {
-            const users = JSON.parse(responseText);
-            const user4 = users[3];
-            const id = user4.id;
-            return id;
-        })
-        .then((id) => {
-            const responseTodos = get(
-                `https://jsonplaceholder.typicode.com/todos?userId=${id}`
-            ).then((responseText) => {
-                const result = JSON.parse(responseText);
-                console.log(result);
-            });
-        })
-        .catch((errorMessage) => {
-            console.log("DANS LE .CATCH");
-            console.error(errorMessage);
-        });
+const getTodoss = () => {
+  const response = get('https://jsonplaceholder.typicode.com/users')
+    .then(responseText => {
+      const users = JSON.parse(responseText);
+      const user4 = users[3];
+      const id = user4.id;
+      return id;
+    })
+    .then(id => {
+      const responseTodos = get(
+        `https://jsonplaceholder.typicode.com/todos?userId=${id}`
+      ).then(responseText => {
+        const result = JSON.parse(responseText);
+        console.log(result);
+      });
+    })
+    .catch(errorMessage => {
+      console.log('DANS LE .CATCH');
+      console.error(errorMessage);
+    });
 };
 
 // Glorian Bikoumou 10 h 21
-const get = (url) => {
-    return new Promise((resolve, reject) => {
-        const xhr = new window.XMLHttpRequest();
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4)
-                if (xhr.status === 200) resolve(xhr.responseText);
-                else reject("Ceci est un message d'erreur");
-        };
-        xhr.open("GET", url);
-        xhr.send();
-    });
+const get = url => {
+  return new Promise((resolve, reject) => {
+    const xhr = new window.XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4)
+        if (xhr.status === 200) resolve(xhr.responseText);
+        else reject("Ceci est un message d'erreur");
+    };
+    xhr.open('GET', url);
+    xhr.send();
+  });
 };
-const getTodos = async() => {
-    const response = await get("https://jsonplaceholder.typicode.com/users");
-    const users = JSON.parse(response);
-    const user = users[3];
-    const id = user.id;
-    const responseTodos = await get(
-        `https://jsonplaceholder.typicode.com/todos?userId=${id}`
-    );
-    const todos = JSON.parse(responseTodos);
-    console.log(todos);
+const getTodos = async () => {
+  const response = await get('https://jsonplaceholder.typicode.com/users');
+  const users = JSON.parse(response);
+  const user = users[3];
+  const id = user.id;
+  const responseTodos = await get(
+    `https://jsonplaceholder.typicode.com/todos?userId=${id}`
+  );
+  const todos = JSON.parse(responseTodos);
+  console.log(todos);
 };
+
+//############################## REACT JS #########################################
--- training.js
+++ training.js
@@ -15,21 +15,21 @@
 // xhr.send();
 console.log(' ##############  Traning.js ##############"');
 
-// 2. recupere l'email de l'utilisateur concerne par cet tache 
+// 2. recupere l'email de l'utilisateur concerne par cet tache
 //  via https: //jsonplaceholder.typicode.com/users
 
-const get = (url) => {
-    return new Promise((resolve, reject) => {
-        const xhr = new window.XMLHttpRequest();
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4)
-                if (xhr.status === 200) resolve(xhr.responseText);
-                else reject("Ceci est un message d'erreur");
-        };
-        xhr.open("GET", url);
-        xhr.send();
-    });
-};
+// const get = (url) => {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new window.XMLHttpRequest();
+//         xhr.onreadystatechange = () => {
+//             if (xhr.readyState === 4)
+//                 if (xhr.status === 200) resolve(xhr.responseText);
+//                 else reject("Ceci est un message d'erreur");
+//         };
+//         xhr.open("GET", url);
+//         xhr.send();
+//     });
+// };
 // const getTodos = async() => {
 //     const response = await get("https://jsonplaceholder.typicode.com/todos");
 //     const todos = JSON.parse(response);
@@ -51,27 +51,69 @@ const get = (url) => {
 //     //     // console.log(todo[]);
 // };
 
-
 // const getUsers = async() => {
 //     const response = await getTodos();
 //     // console.log(userId.)
 // }
 //  ************* Correction Glorian **************************
-const getTodos = async() => {
-    const response = await get("https://jsonplaceholder.typicode.com/todos");
-    const todos = JSON.parse(response);
-    const todo = todos[41];
-    // console.log(todos.filter((todo) => todo.id === 42));
-    const userId = todo.userId;
-    // console.log(userId);
-    const responseUser = await get(
-        `https://jsonplaceholder.typicode.com/users?id=${userId}`
-    );
-    // console.log(responseUser);
-    const parseResponse = JSON.parse(responseUser);
-    const user = parseResponse[0];
-    const email = user.email;
-    console.log(email);
-};
+// const getTodos = async() => {
+//     const response = await get("https://jsonplaceholder.typicode.com/todos");
+//     const todos = JSON.parse(response);
+//     const todo = todos[41];
+//     // console.log(todos.filter((todo) => todo.id === 42));
+//     const userId = todo.userId;
+//     // console.log(userId);
+//     const responseUser = await get(
+//         `https://jsonplaceholder.typicode.com/users?id=${userId}`
+//     );
+//     // console.log(responseUser);
+//     const parseResponse = JSON.parse(responseUser);
+//     const user = parseResponse[0];
+//     const email = user.email;
+//     console.log(email);
+// };
 
-getTodos();
+// getTodos();
+
+class Personne {
+  constructor(nom, prenom) {
+    this.nom = nom;
+    this.prenom = prenom;
+  }
+}
+var personne = new Personne('John', 'Wick');
+
+class Commercial extends Personne {
+  constructor(nom, prenom, client) {
+    super(nom, prenom);
+    this.client = client;
+  }
+}
+var commercial = new Commercial('John', 'Wick', 'ThermoMix');
+
+// const people = {
+//     firstName = 'Tony',
+//     lastName = 'Stark'
+// };
+
+// let {firstName, lastName} = people;
+
+var datas = [3, 4, 5];
+var items = [1, 2, ...datas];
+var a = { prop: 'val' };
+var b = { ...a };
+b.prop = false;
+console.log(a, b);
+
+const bonjour = (nom = 'anonyme', message = 'bonjour') => message + ' ' + nom;
+console.log(bonjour());
+console.log(bonjour('John'));
+console.log(bonjour('John', 'Salut'));
+
+function somme(...parames) {
+    return parames.reduce((accumulateur, element) => {
+       return accumulateur + element;
+    });
+}
+console.log(somme(1));
+console.log(somme(1,2,3,4,5));
